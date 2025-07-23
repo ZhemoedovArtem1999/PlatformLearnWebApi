3abdb24d41be43415d0a2cbaaf84b4b57b137b05
Zhemoedov <tema.zhemoedov1999@gmail.com>
2025-07-23 04:52:31 +0300

    Добавление в меню Выйти логика появления кнопок есть ли токен или нет

--- Source/client-learn-ui/src/components/common/Header/Header.tsx
+++ Source/client-learn-ui/src/components/common/Header/Header.tsx
@@ -1,58 +1,69 @@
-import React from 'react';
-import { Menubar } from 'primereact/menubar';
-import { MenuItem } from 'primereact/menuitem'; // Явный импорт типа
-import { MegaMenu } from 'primereact/megamenu';
-import { Logo } from './Logo';
-
-
+import React from "react";
+import { Menubar } from "primereact/menubar";
+import { MenuItem } from "primereact/menuitem";
+import { Logo } from "./Logo";
+import authStore from "../../../stores/auth-store";
 
 interface IHeaderProps {
-    item?: string;
+  item?: string;
 }
 
 export const Header: React.FC<IHeaderProps> = (props: IHeaderProps) => {
 
-    const items: MenuItem[] = [
+  const logout = () => {
+    authStore.clearAuth();
+  };
+
+  const items: MenuItem[] = [
+    {
+      label: "Поддержка",
+      url: "/",
+      className: "menu-item",
+    },
+    {
+      label: "Каталог",
+      items: [
         {
-            label: "Поддержка",
-            url: "/",
-            className: "menu-item",
+          label: "Программирование",
+          url: "/",
+          className: "menu-item",
         },
         {
-            label: "Каталог",
-            items: [
-                {
-                    label: "Программирование",
-                    url: "/",
-                    className: "menu-item"
-                },
-                {
-                    label: "Дизайн",
-                    url: "/",
-                    className: "menu-item"
-                }
-            ],
-            className: "menu-item",
-        }, {
-            label: "Войти",
-            url: "/auth",
-            className: "menu-item",
+          label: "Дизайн",
+          url: "/",
+          className: "menu-item",
         },
-
-    ];
-    return (
-        <div className="header-wrapper">
-            <div className="header-content" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Logo />
-                <Menubar
-                    model={items}
-                    className="custom-menubar"
-                    style={{ display: 'flex', justifyContent: "flex-end" }}
-                />
-            </div>
-        </div>
-
-    );
+      ],
+      className: "menu-item",
+    },
+    {
+      label: "Войти",
+      url: "/auth",
+      className: "menu-item",
+      visible: !localStorage.isAuth(),
+    },
+    {
+      label: "Выйти",
+      command: logout,
+      className: "menu-item",
+      visible: localStorage.isAuth(),
+    },
+  ];
+  return (
+    <div className="header-wrapper">
+      <div
+        className="header-content"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Logo />
+        <Menubar
+          model={items}
+          className="custom-menubar"
+          style={{ display: "flex", justifyContent: "flex-end" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- Source/client-learn-ui/src/stores/auth-store.tsx
+++ Source/client-learn-ui/src/stores/auth-store.tsx
@@ -1,54 +1,59 @@
 import { makeAutoObservable, action } from "mobx";
 
 class AuthStore {
-    accessToken: string | null = null;
-    userName: string | null = null;
-    errorMessage: string | null = null;
-    isLoading: boolean = false;
+  accessToken: string | null = null;
+  userName: string | null = null;
+  errorMessage: string | null = null;
+  isLoading: boolean = false;
 
-    constructor() {
-        makeAutoObservable(this);
-        this.loadFromLocalStorage();
+  constructor() {
+    makeAutoObservable(this);
+    this.loadFromLocalStorage();
+  }
+
+  // Сохраняет данные авторизации в localStorage
+  @action
+  saveToLocalStorage() {
+    localStorage.setItem(
+      "auth",
+      JSON.stringify({
+        accessToken: this.accessToken,
+        userName: this.userName,
+      })
+    );
+  }
+
+  // Загружает данные авторизации из localStorage
+  @action
+  loadFromLocalStorage() {
+    const savedAuth = localStorage.getItem("auth");
+    if (savedAuth) {
+      const { accessToken, userName, rsmSpaAccessToken } =
+        JSON.parse(savedAuth);
+      this.accessToken = accessToken;
+      this.userName = userName;
     }
+  }
+  @action
+  setPlatformLearnAuth(token: string, name: string) {
+    this.accessToken = token;
+    this.userName = name;
+    this.saveToLocalStorage(); // Сохраняем данные в localStorage
+  }
 
-    // Сохраняет данные авторизации в localStorage
-    @action
-    saveToLocalStorage() {
-        localStorage.setItem(
-            "auth",
-            JSON.stringify({
-                accessToken: this.accessToken,
-                userName: this.userName,
-            })
-        );
-    }
+  // Очищает токен и имя пользователя
+  @action
+  clearAuth() {
+    this.accessToken = null;
+    this.userName = null;
+    localStorage.removeItem("auth");
+  }
 
-    // Загружает данные авторизации из localStorage
-    @action
-    loadFromLocalStorage() {
-        const savedAuth = localStorage.getItem("auth");
-        if (savedAuth) {
-            const { accessToken, userName, rsmSpaAccessToken } = JSON.parse(savedAuth);
-            this.accessToken = accessToken;
-            this.userName = userName;
-        }
-    }
-    @action
-    setPlatformLearnAuth(token: string, name: string) {
-        this.accessToken = token;
-        this.userName = name;
-        this.saveToLocalStorage(); // Сохраняем данные в localStorage
-    }
-
-    // Очищает токен и имя пользователя
-    @action
-    clearAuth() {
-        this.accessToken = null;
-        this.userName = null;
-        localStorage.removeItem("auth");
-    }
-
-
+  @action
+  isAuth = (): boolean => {
+    const token = authStore.accessToken;
+    return token ? true : false;
+  };
 }
 
 const authStore = new AuthStore();
