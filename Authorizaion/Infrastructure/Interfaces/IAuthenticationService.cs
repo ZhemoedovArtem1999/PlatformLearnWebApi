9016660abba667643c3fd18875f2c3385757ddd8
Zhemoedov <tema.zhemoedov1999@gmail.com>
2025-07-23 09:19:52 +0300

    Удаление лишнего неиспользуемого проекта

--- Authorizaion/Infrastructure/Interfaces/IAuthenticationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Authentication.Models;
-
-namespace Authentication.Infrastructure.Interfaces
-{
-    public interface IAuthenticationService
-    {
-        Task<AuthentcationResponseDto> LoginAsync(AuthentcationDto authentcation, CancellationToken cancellationToken = default);
-    }
-}
